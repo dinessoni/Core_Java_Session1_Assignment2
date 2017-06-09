3376b350e3d043711371654389017cff6acb0fc4
Dinesh Soni <dinessoni@gmail.com>
2017-06-09 10:41:22 +0530

    Printing the sum of two numbers
    Adding two numbers without using the operator +

---
+++ src/se01as02/addnum/AddMain.java
@@ -0,0 +1,13 @@
+package se01as02.addnum;
+
+public class AddMain {
+
+	public static void main(String[] args) {
+		AddNum add = new AddNum();
+		add.num1 = 4;
+		add.num2 = 3;
+		add.sum();
+
+	}
+
+}
---
+++ src/se01as02/addnum/AddNum.java
@@ -0,0 +1,14 @@
+package se01as02.addnum;
+
+public class AddNum {
+	int num1;
+	int num2;
+	
+	public void sum()
+	{
+		int num3;
+		num3= (num1)-(-num2);
+		System.out.println("The sum of given numbers is : "+num3);
+		
+	}
+}
